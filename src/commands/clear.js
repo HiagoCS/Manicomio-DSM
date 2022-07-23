c84ca6fc979db30b1901336d5cfa6560c87fc207
HiagoCS <hiago8cs@gmail.com>
2022-07-23 07:58:24 -0300

    delete log messages after 5s

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -8,26 +8,38 @@ module.exports = {
                 const cId = args.replace(/\D/g, '');
                 const channel = bot.guilds.cache.first().channels.cache.find(c => c.id === cId);
                 if(!channel)
-                    return msg.channel.send({content:`Canal não existe!`});
+                    return msg.channel.send({content:`Canal não existe!`})
+                    .then(x =>{
+                        setTimeout(() => x.delete(), 5000)
+                    }).catch(console.error);
                 let size = 0;
                 channel.messages.fetch()
                 .then(x =>{
                     x.map(y =>{size++;});
                     channel.bulkDelete(size);
-                    msg.channel.send({content:size>1?`Foram deletadas ${size} mensagens no canal ${channel.name}!`:`Apenas uma mensagem foi deletada no canal ${channel.name}`});
+                    msg.channel.send({content:size>1?`Foram deletadas ${size} mensagens no canal ${channel.name}!`:`Apenas uma mensagem foi deletada no canal ${channel.name}`})
+                        .then(x =>{
+                            setTimeout(() => x.delete(), 5000)
+                        }).catch(console.error);
                 }).catch(console.error);
             }
             else if(args.includes("@") && args.includes("<") && args.includes(">") && !args.includes("#")){
                 const uId = args.replace(/\D/g, '');
                 const user = bot.guilds.cache.first().members.cache.find(u => u.id === uId).user;
                 if(!user)
-                    return msg.channel.send({content:`Este usuário não existe!`});
+                    return msg.channel.send({content:`Este usuário não existe!`})
+                    .then(x =>{
+                        setTimeout(() => x.delete(), 5000)
+                    }).catch(console.error);
                 msg.channel.messages.fetch()
                 .then(x =>{
                     const array = [];
                     x.filter(m => m.author.id === user.id).forEach(message => array.push(message));
                     msg.channel.bulkDelete(array);
-                    msg.channel.send({content:`Todas as mensagens de ${user.username} foram deletadas!`});
+                    msg.channel.send({content:`Todas as mensagens de ${user.username} foram deletadas!`})
+                    .then(x =>{
+                        setTimeout(() => x.delete(), 5000)
+                    }).catch(console.error);
                 }).catch(console.error);
             }
             else if(args.includes("@") && args.includes("<") && args.includes(">") && args.includes("#")){
@@ -44,13 +56,19 @@ module.exports = {
                 const channel = bot.guilds.cache.first().channels.cache.find(c => c.id === cId);
                 const user = bot.guilds.cache.first().members.cache.find(u => u.id === uId).user;
                 if(!user)
-                    return msg.channel.send({content:`Este usuário não existe!`});
+                    return msg.channel.send({content:`Este usuário não existe!`})
+                    .then(x =>{
+                        setTimeout(() => x.delete(), 5000)
+                    }).catch(console.error);
                 channel.messages.fetch()
                 .then(x =>{
                     const array = [];
                     x.filter(m => m.author.id === user.id).forEach(message => array.push(message));
                     channel.bulkDelete(array);
-                    msg.channel.send({content:`Todas as mensagens do canal ${channel.name} e do usuário ${user.username} foram deletadas!`});
+                    msg.channel.send({content:`Todas as mensagens do canal ${channel.name} e do usuário ${user.username} foram deletadas!`})
+                    .then(x =>{
+                        setTimeout(() => x.delete(), 5000)
+                    }).catch(console.error);
                 }).catch(console.error);
             }
         }
@@ -60,7 +78,10 @@ module.exports = {
                 .then(x =>{
                     x.map(y =>{size++;});
                     msg.channel.bulkDelete(size);
-                    msg.channel.send({content:size>1?`Foram deletadas ${size} mensagens!`:`Apenas uma mensagem foi deletada`});
+                    msg.channel.send({content:size>1?`Foram deletadas ${size} mensagens!`:`Apenas uma mensagem foi deletada`})
+                    .then(x =>{
+                        setTimeout(() => x.delete(), 5000)
+                    }).catch(console.error);
                 }).catch(console.error);
         }
     },
